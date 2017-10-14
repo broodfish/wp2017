4c7c25de98afc3d9c0a9dcc3e4c60bdc5ab45ebb
szyu <sag860610@gmail.com>
2017-10-15 03:07:41 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
     if(x==1){
       $("#profile").animate({marginTop:"-10vh"}).transition('fade down');
       $("#contact").animate({marginTop:"5vh"});
-      $("#pic").animate({marginTop:"35vh"}).transition('fade up');
+      $("#pic").animate({marginTop:"60vh"}).transition('fade up');
       $("#profile").animate({marginTop:"110vh"});
       x+=1;
     }
     else if(x==2){
       $("#contact").animate({marginTop:"-10vh"}).transition('fade down'); 
       $("#pic").animate({marginTop:"5vh"});
-      $("#profile").animate({marginTop:"35vh"}).transition('fade up');
+      $("#profile").animate({marginTop:"60vh"}).transition('fade up');
       $("#contact").animate({marginTop:"110vh"});
       x+=1;
     }
     else if(x==3){
       $("#pic").animate({marginTop:"-10vh"}).transition('fade down');
       $("#profile").animate({marginTop:"5vh"});
-      $("#contact").animate({marginTop:"35vh"}).transition('fade up');
+      $("#contact").animate({marginTop:"60vh"}).transition('fade up');
       $("#pic").animate({marginTop:"110vh"});
       x=1;
     }
