fe3da3ef8dbcbd06679424e0349e07ba60e17e39
szyu <sag860610@gmail.com>
2017-10-14 23:21:05 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -7,11 +7,13 @@ $(document).ready(function(){
     }
     else if(x==2){
       $("#profile").transition('fly down');
+      $("#profile").hide(20);
       $("#contact").transition('fly up');
       x+=1;
     }
     else if(x==3){
       $("#contact").transition('fly down');
+      $("#contact").hide(20);
       x=1;
     }
   });
