e93b8cde8c5bdfeb304357a7cb413b53f4869280
szyu <sag860610@gmail.com>
2017-10-15 03:04:26 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -2,24 +2,24 @@ var x=1;
 $(document).ready(function(){
   $(document).click(function(){
     if(x==1){
-      $("#profile").animate({marginTop:"-10%"}).transition('fade down');
-      $("#contact").animate({marginTop:"5%"});
-      $("#pic").animate({marginTop:"35%"}).transition('fade up');
-      $("#profile").animate({marginTop:"110%"});
+      $("#profile").animate({marginTop:"-10vh"}).transition('fade down');
+      $("#contact").animate({marginTop:"5vh"});
+      $("#pic").animate({marginTop:"35vh"}).transition('fade up');
+      $("#profile").animate({marginTop:"110vh"});
       x+=1;
     }
     else if(x==2){
-      $("#contact").animate({marginTop:"-10%"}).transition('fade down'); 
-      $("#pic").animate({marginTop:"5%"});
-      $("#profile").animate({marginTop:"35%"}).transition('fade up');
-      $("#contact").animate({marginTop:"110%"});
+      $("#contact").animate({marginTop:"-10vh"}).transition('fade down'); 
+      $("#pic").animate({marginTop:"5vh"});
+      $("#profile").animate({marginTop:"35vh"}).transition('fade up');
+      $("#contact").animate({marginTop:"110vh"});
       x+=1;
     }
     else if(x==3){
-      $("#pic").animate({marginTop:"-10%"}).transition('fade down');
-      $("#profile").animate({marginTop:"5%"});
-      $("#contact").animate({marginTop:"35%"}).transition('fade up');
-      $("#pic").animate({marginTop:"110%"});
+      $("#pic").animate({marginTop:"-10vh"}).transition('fade down');
+      $("#profile").animate({marginTop:"5vh"});
+      $("#contact").animate({marginTop:"35vh"}).transition('fade up');
+      $("#pic").animate({marginTop:"110vh"});
       x=1;
     }
   });
