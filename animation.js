546a454268a40322f62a7e47c8406cdb19ae30ea
szyu <sag860610@gmail.com>
2017-10-15 02:17:34 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -4,21 +4,21 @@ $(document).ready(function(){
     if(x==1){
       $("#profile").animate({marginTop:"-10%"},"slow").transition('fade down');
       $("#contact").animate({marginTop:"15%"},"slow");
-      $("#pic").animate({marginTop:"60%"},"slow").transition('fade up');
+      $("#pic").animate({marginTop:"70%"},"slow").transition('fade up');
       $("#profile").animate({marginTop:"110%"});
       x+=1;
     }
     else if(x==2){
       $("#contact").animate({marginTop:"-10%"},"slow").transition('fade down'); 
       $("#pic").animate({marginTop:"15%"},"slow");
-      $("#profile").animate({marginTop:"60%"},"slow").transition('fade up');
+      $("#profile").animate({marginTop:"70%"},"slow").transition('fade up');
       $("#contact").animate({marginTop:"110%"});
       x+=1;
     }
     else if(x==3){
       $("#pic").animate({marginTop:"-10%"},"slow").transition('fade down');
       $("#profile").animate({marginTop:"15%"},"slow");
-      $("#contact").animate({marginTop:"60%"},"slow").transition('fade up');
+      $("#contact").animate({marginTop:"70%"},"slow").transition('fade up');
       $("#pic").animate({marginTop:"110%"});
       x=1;
     }
