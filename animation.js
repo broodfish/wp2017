e416f643ecebe3ab2b2d9f637fb34ce2e2cff6de
szyu <sag860610@gmail.com>
2017-10-15 02:24:00 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -2,23 +2,23 @@ var x=1;
 $(document).ready(function(){
   $(document).click(function(){
     if(x==1){
-      $("#profile").animate({marginTop:"-10%"},"slow").transition('fade down');
-      $("#contact").animate({marginTop:"15%"},"slow");
-      $("#pic").animate({marginTop:"70%"},"slow").transition('fade up');
+      $("#profile").animate({marginTop:"-10%"}).transition('fade down');
+      $("#contact").animate({marginTop:"5%"});
+      $("#pic").animate({marginTop:"50%"}).transition('fade up');
       $("#profile").animate({marginTop:"110%"});
       x+=1;
     }
     else if(x==2){
-      $("#contact").animate({marginTop:"-10%"},"slow").transition('fade down'); 
-      $("#pic").animate({marginTop:"15%"},"slow");
-      $("#profile").animate({marginTop:"70%"},"slow").transition('fade up');
+      $("#contact").animate({marginTop:"-10%"}).transition('fade down'); 
+      $("#pic").animate({marginTop:"5%"});
+      $("#profile").animate({marginTop:"50%"}).transition('fade up');
       $("#contact").animate({marginTop:"110%"});
       x+=1;
     }
     else if(x==3){
-      $("#pic").animate({marginTop:"-10%"},"slow").transition('fade down');
-      $("#profile").animate({marginTop:"15%"},"slow");
-      $("#contact").animate({marginTop:"70%"},"slow").transition('fade up');
+      $("#pic").animate({marginTop:"-10%"}).transition('fade down');
+      $("#profile").animate({marginTop:"5%"});
+      $("#contact").animate({marginTop:"50%"}).transition('fade up');
       $("#pic").animate({marginTop:"110%"});
       x=1;
     }
