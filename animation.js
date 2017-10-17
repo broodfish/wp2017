5b5c339a18753d8adecd364ce01caa49a1cf0060
szyu <sag860610@gmail.com>
2017-10-18 02:14:47 +0800

    Openshift

--- animation.js
+++ animation.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   $(window).mousewheel(function(e){
     b1=0;
     b2=0;
-    b3=0
+    b3=0;
     $(".box1>.content").hide("fast",function(){$(".box1").animate({height:"40vh", width:"40vw", margin:"2.5vw auto 5vh 5vh"},"fast");});
     $(".box2>.content").hide("fast",function(){$(".box2").animate({height:"40vh", width:"35vw", margin:"15vh 10vh 25vh auto", float: "right"},"fast");});
     $(".box3>.content").hide("fast",function(){$(".box3").animate({height:"60vh", width:"45vw", margin:"10vh auto 20vh 35vh"},"fast");}); 
@@ -62,6 +62,12 @@ $(document).ready(function(){
   });
   var b1=0,b2=0,b3=0;
   $(".box1").click(function(){
+    b1=0;
+    b2=0;
+    b3=0;
+    $(".box1>.content").hide("fast",function(){$(".box1").animate({height:"40vh", width:"40vw", margin:"2.5vw auto 5vh 5vh"},"fast");});
+    $(".box2>.content").hide("fast",function(){$(".box2").animate({height:"40vh", width:"35vw", margin:"15vh 10vh 25vh auto", float: "right"},"fast");});
+    $(".box3>.content").hide("fast",function(){$(".box3").animate({height:"60vh", width:"45vw", margin:"10vh auto 20vh 35vh"},"fast");}); 
     if(b1==0){
       $(".box1").animate({height:"80vh", width:"70vw"},function(){$(".box1>.content").show();});
       b1=1;
